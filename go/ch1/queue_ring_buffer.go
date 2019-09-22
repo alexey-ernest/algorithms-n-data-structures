4eb03e93b89c5815a88c3d65e76c84e12e06395d
alexey-ernest <alexey.ernest@gmail.com>
2019-09-22 14:17:49 -0700

    ring buffer queue

---
+++ go/ch1/queue_ring_buffer.go
@@ -0,0 +1,52 @@
+package ch1
+
+type queueRingBuffer struct {
+	data []string
+	start int
+	end int
+	n int
+}
+
+func NewQueueRingBuffer(size int) queueRingBuffer {
+	buf := make([]string, size, size);
+	q := queueRingBuffer {
+		data: buf,
+	};
+
+	return q;
+}
+
+func (q *queueRingBuffer) IsEmpty() bool {
+	return q.n == 0;
+}
+
+func (q *queueRingBuffer) Size() int {
+	return q.n;
+}
+
+func (q *queueRingBuffer) Enqueue(s string) {
+	// blocking until we have enough space
+	for q.n == cap(q.data) {}
+
+	q.data[q.end] = s;
+	q.end += 1;
+	if q.end == cap(q.data) {
+		q.end = 0;
+	}
+
+	q.n += 1;
+}
+
+func (q *queueRingBuffer) Dequeue() string {
+	// block until we have some work
+	for q.n == 0 {}
+
+	e := q.data[q.start];
+	q.start += 1;
+	if q.start == cap(q.data) {
+		q.start = 0;
+	}
+
+	q.n -= 1;
+	return e;
+}
---
+++ go/ch1/queue_ring_buffer_test.go
@@ -0,0 +1,54 @@
+package ch1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestQueueRingBuffer(t *testing.T) {
+	q := NewQueueRingBuffer(10);
+	if q.IsEmpty() != true {
+		t.Errorf("newly created queue should be empty");
+	}
+	if q.Size() != 0 {
+		t.Errorf("newly created queue should has Size() == 0");
+	}
+}
+
+func TestQueueNDequeueRingBuffer(t *testing.T) {
+	q := NewQueueRingBuffer(10);
+	el := "abc";
+	q.Enqueue(el);
+
+	got := q.Dequeue();
+	if got != "abc" {
+		t.Errorf("%q != %q", el, got);
+	}
+}
+
+func TestQueueNDequeueWrapAroundRingBuffer(t *testing.T) {
+	var a [20]string;
+	q := NewQueueRingBuffer(10);
+	for i := 0; i < 10; i+=1 {
+		a[i] = fmt.Sprintf("%s", string('a' + i));
+		q.Enqueue(a[i]);
+	}
+
+	var b [20]string;
+	for i := 0; i < 5; i +=1 {
+		b[i] = q.Dequeue();
+	}
+
+	for i := 10; i < 15; i+=1 {
+		a[i] = fmt.Sprintf("%s", string('a' + i));
+		q.Enqueue(a[i]);
+	}
+
+	for i := 5; i < 15; i +=1 {
+		b[i] = q.Dequeue();
+	}
+
+	if a != b {
+		t.Errorf("%q != %q", a, b);
+	}
+}
