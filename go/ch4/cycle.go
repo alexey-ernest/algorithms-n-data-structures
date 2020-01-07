296f55df7133bb01d3182dfa4826e3d5bb1ec861
alexey-ernest <alexey.ernest@gmail.com>
2020-01-06 19:39:39 -0800

    cycle detection

---
+++ go/ch4/cycle.go
@@ -0,0 +1,28 @@
+package ch4
+
+func IsAcyclic(adj [][]int) bool {
+
+	marked := make([]bool, len(adj))
+	acyclic := true
+
+	var dfs func(int, int)
+	dfs = func (v int, u int) {
+		marked[v] = true
+		for _, w := range adj[v] {
+			if !marked[w] {
+				dfs(w, v)
+			} else if w != u {
+				acyclic = false
+			}
+		}
+	}
+
+	for i := range adj {
+		if marked[i] {
+			continue
+		}
+		dfs(i, i)
+	}
+
+	return acyclic
+}
